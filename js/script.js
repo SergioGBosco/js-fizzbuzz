07a1dda58d8c4511eaee87c632ec074ec4dbb494
SergioGBosco <sergiogibosco@gmail.com>
2025-07-02 14:49:08 +0200

    [Add] IF for the first requirement

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 // Definisco l'intervallo di numeri per far partire il gioco utilizzando il ciclo For 
 
-for (let i = 1; i <= 100; i++)
-  console.log(i) 
+for (let i = 1; i <= 100; i++) {
+
+
+  // Stabilisco che per ogni numero divisibile sia per 3 che per 5 restituisci come resto 0 esce la parola FizzBuzz
+
+  if (i % 5 === 0 && i % 3 === 0) {
+    console.log(`FizzBuzz`)
+  }
+
+
+}
